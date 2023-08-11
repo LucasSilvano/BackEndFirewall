03d4a9744fdca4c5f3e6c2171e7d6d0de8f5cf1e
Lucas Silvano <silvanolucas000@gmail.com>
2023-08-11 15:51:40 -0400

    Aplicação concluída, iniciar refatoração

--- App/Utils/Config.php
+++ App/Utils/Config.php
@@ -1,15 +1,5 @@
 <?php
 
-// Definir as configurações do banco de dados
-
-
-DEFINE('DB_HOST', '100.64.145.183');
-DEFINE('DB_NAME', 'teste');
-DEFINE('DB_USER', 'teste');
-DEFINE('DB_PASS', 'admin');
-DEFINE('DB_CHARSET', 'utf8mb4')
-
-
 
 
 ?>
